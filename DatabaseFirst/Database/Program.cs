f76f671ff804dfeca59f76fb053c47463fe05083
BOMBUSVS <vsevoloddesign8@gmail.com>
2021-10-26 19:01:23 +0300

    ADO.NET Entity Framework
    ADO.NET Entity Framework : CodeFirst, ModelFirst, DataBaseFirst.

---
+++ DatabaseFirst/Database/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using( var db = new StudyModelFirstEntities ())
+            {
+                var class_id = new School()
+                {
+                    Subjects = "Math",
+                    Teachers = "Marina Olexandrivna"
+              };
+               
+                var class_id2 = new School()
+                {
+                    Subjects = "English",
+                    Teachers = "Natalia Simonenko"
+                };
+
+                var Student_id = new Student()
+                {
+                    Name = "Igor",
+                    Age = 17
+                };
+
+                db.Students.Add(Student_id);
+                db.SchoolS.Add(class_id2);
+                db.SchoolS.Add(class_id);
+                db.SaveChanges();
+
+
+            }
+
+        }
+    }
+}
---
+++ ModelFirst/Ef_ModelFirst/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ef_ModelFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AccountingSystemContainer())
+            {
+                var invHeader = db.InvoiceHeaders.Create();
+                var invDetail = db.InvoiceDetailSet.Create();
+
+                invHeader.Total = 150m;
+
+                invDetail.ItemDescription = "Some Item";
+                invDetail.Price = 75m;
+                invDetail.Quantity = 2;
+
+                var invDetails = db.InvoiceDetailSet.Add(invDetail);
+
+                db.InvoiceHeaders.Add(invHeader);
+                db.SaveChanges();
+
+
+               
+
+                
+            }
+        }
+    }
+}
